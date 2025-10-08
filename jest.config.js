f9de10733a4c7b47825a17917d59bcd6412fd9a1
Simon Smilga <simon0x1800@gmail.com>
2025-10-08 23:18:44 +0000

    feat: Add unit tests for Twitter API modules

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\.ts?$': 'babel-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  modulePaths: ['<rootDir>/node_modules'],
+};
